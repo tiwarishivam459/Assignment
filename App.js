f1547eaebe68ce1c2f31e215c798b85c8d23eb2b
Shivam Tiwari <95228896+tiwarishivam459@users.noreply.github.com>
2023-01-17 16:31:54 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Assignment from './components/Assignment/Assignment';
+
+function App() {
+  console.log('App');
+  return (
+    <div className="App">
+    <div id="box">
+     <Assignment />
+      </div>
+    </div>
+  );
+}
+
+export default App;
